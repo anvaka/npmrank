d32b9c4cc367d0a478e15eb99cbc7951f7059a4f
anvaka <anvaka@gmail.com>
2015-04-02 21:01:14 -0700

    Restructured, preparing for reuse

--- recommend.js
+++ recommend.js
@@ -1,14 +1,26 @@
-var fs = require('fs');
 var prompt = require('prompt');
-var tags = collectTags();
-var rank = computePageRank();
+
+console.log('Collecting tags...');
+var collectTags = require('./lib/collectTags.js');
+var result = collectTags('./data/byField.in.graph');
+var tags = result.tags;
+console.log('Collected ' + result.totalTags + '; Unique tags: ' + result.uniqueTags);
+
+
+console.log('Computing page rank...');
+var computePageRank = require('./lib/computePageRank.js');
+var rank = computePageRank('./data/dependenciesGraph.out.graph');
+console.log('Done');
 
 prompt.start();
 getNextInput();
 
 function getNextInput() {
-  prompt.get(['tag'], function (err, result) {
-    var matches = (tags[result.tag] || []).sort(byPagerank);
+  prompt.get([{
+    description: 'Enter package keyword',
+    name: 'keyword'
+  }], function (err, result) {
+    var matches = (tags[result.keyword] || []).sort(byPagerank);
     console.log('----------------------------------------------------------------------');
     console.log(matches.slice(0, Math.min(20, matches.length)).map(toDisplay).join('\n'));
     getNextInput();
@@ -21,55 +33,4 @@ function toDisplay(x) {
 
 function byPagerank(x, y) {
   return rank[y] - rank[x];
-}
-
-function collectTags() {
-  console.log('collecting tags');
-  var fileName = './data/byField.in.graph';
-  var inputFile = JSON.parse(fs.readFileSync(fileName, 'utf8'));
-  var packages = inputFile.rows;
-  var collectedTags = Object.create(null);
-  var totalTags = 0;
-  var uniqueTags = 0;
-  var empty = [];
-
-  packages.forEach(addNode);
-  console.log("Collected " + totalTags + " tags; Unique: " + uniqueTags);
-  return collectedTags;
-
-  function addNode(pkg) {
-    var id = pkg.id;
-    accumulate(pkg.value.tags);
-    accumulate(pkg.value.keywords);
-
-    function accumulate(tags) {
-      tags = tags || empty;
-      if (typeof tags === 'string') {
-        tags = tags.split(/[\s,]/g);
-      }
-      tags.forEach(recordTag);
-    }
-
-    function recordTag(tag) {
-      if (!tag || tag === '__proto__') {
-        return;
-      }
-      var packages = collectedTags[tag];
-      if (!packages) {
-        collectedTags[tag] = [id];
-        uniqueTags += 1;
-      } else {
-        packages.push(id);
-      }
-      totalTags += 1;
-    }
-  }
-}
-
-function computePageRank() {
-  console.log('computing page rank');
-  var pagerank = require('ngraph.pagerank');
-  var fromjson = require('ngraph.fromjson');
-  var graph = fromjson(fs.readFileSync('./data/dependenciesGraph.out.graph', 'utf8'));
-  return pagerank(graph);
 }
