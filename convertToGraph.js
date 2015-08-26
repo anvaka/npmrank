d165f8dfa072a9655006c540cd1ebf27334fd165
anvaka <anvaka@gmail.com>
2015-08-25 23:05:04 -0700

    Using streams to parse json file
    Original approach with plain utf8 string parsing is failing due to
    https://github.com/joyent/node/issues/25687
    
    This change is hack to fix https://github.com/anvaka/npmrank/issues/2

--- convertToGraph.js
+++ convertToGraph.js
@@ -2,77 +2,77 @@
  * Converts npm registry `byField` response into serialized graph
  */
 var fs = require('fs');
-var inputFileName = process.argv[2] || './data/byField.in.graph';
-var packages = readInputFile(inputFileName);
 var createGraph = require('ngraph.graph');
 var tojson = require('ngraph.tojson');
+var load = require('./lib/load.js');
 
-saveGraph('dependencies', './data/dependenciesGraph.out.graph');
-saveGraph('devDependencies', './data/devDependencies.out.graph');
-saveGraph('allDependencies', './data/allDependencies.out.graph');
+var inputFileName = process.argv[2] || './data/byField.in.graph';
+console.log('Parsing npm packages file...');
+load(inputFileName, function(packages) {
+  saveGraph('dependencies', './data/dependenciesGraph.out.graph');
+  saveGraph('devDependencies', './data/devDependencies.out.graph');
+  saveGraph('allDependencies', './data/allDependencies.out.graph');
 
-console.log('');
-console.log('# How come different graphs have different number of nodes?');
-console.log('Turns out some packages declared dependencies on something which already');
-console.log('does not exist');
-console.log('');
-console.log('For example, take a look at https://www.npmjs.com/package/jolokia-client');
-console.log('It has dependency on package called ` optimist`');
-console.log('Have you noticed extra whitespace    ^ here?');
-console.log('That is why graph has different number of nodes');
-console.log('');
-console.log("All done.");
+  console.log('');
+  // Sometimes different graphs have different number of nodes. Why?
+  // Turns out some packages declared dependencies on something which already
+  // does not exist
+  //
+  // For example, take a look at https://www.npmjs.com/package/jolokia-client
+  // It has dependency on package called ` optimist`
+  // Have you noticed extra whitespace    ^ here?
+  // That is why graph has different number of nodes
+  console.log("All done.");
 
-function saveGraph(kind, fileName) {
-  console.log("Saving `" + kind + '` graph as ' + fileName);
-  var graph = createGraph({ uniqueLinkId: false });
+  return; // public part is done here.
 
-  packages.forEach(addNode);
+  function saveGraph(kind, fileName) {
+    console.log("Saving `" + kind + '` graph as ' + fileName);
+    var graph = createGraph({
+      uniqueLinkId: false
+    });
 
-  fs.writeFileSync(fileName, tojson(graph));
-  console.log("Done. Saved " + graph.getNodesCount() + ' nodes and ' + graph.getLinksCount() + ' edges');
-  return;
+    packages.forEach(addNode);
 
-  function addNode(pkg) {
-    graph.addNode(pkg.id);
-    var deps = getDpendencies(pkg.value, kind);
-    if (deps) {
-      Object.keys(deps).forEach(addLink);
-    }
+    fs.writeFileSync(fileName, tojson(graph));
+    console.log("Done. Saved " + graph.getNodesCount() + ' nodes and ' + graph.getLinksCount() + ' edges');
+    return;
 
-    function addLink(key) {
-      graph.addLink(pkg.id, key);
-    }
-  }
-}
+    function addNode(pkg) {
+      graph.addNode(pkg.id);
+      var deps = getDpendencies(pkg.value, kind);
+      if (deps) {
+        Object.keys(deps).forEach(addLink);
+      }
 
-function getDpendencies(pkg, kind) {
-  if (kind === 'dependencies') {
-    return pkg.dependencies;
-  }
-  if (kind === 'devDependencies') {
-    return pkg.devDependencies;
-  }
-
-  var deps = pkg.dependencies || {};
-  var devDeps = pkg.devDependencies || {};
-  var result = {};
-  for (var key in deps) {
-    if (deps.hasOwnProperty(key)) {
-      result[key] = 1;
-    }
-  }
-  for (key in devDeps) {
-    if (devDeps.hasOwnProperty(key)) {
-      result[key] = 1;
+      function addLink(key) {
+        graph.addLink(pkg.id, key);
+      }
     }
   }
 
-  return result;
-}
+  function getDpendencies(pkg, kind) {
+    if (kind === 'dependencies') {
+      return pkg.dependencies;
+    }
+    if (kind === 'devDependencies') {
+      return pkg.devDependencies;
+    }
 
-function readInputFile(fileName) {
-  console.log("Reading input file " + fileName);
-  var inputFile = JSON.parse(fs.readFileSync(fileName, 'utf8'));
-  return inputFile.rows;
-}
+    var deps = pkg.dependencies || {};
+    var devDeps = pkg.devDependencies || {};
+    var result = {};
+    for (var key in deps) {
+      if (deps.hasOwnProperty(key)) {
+        result[key] = 1;
+      }
+    }
+    for (key in devDeps) {
+      if (devDeps.hasOwnProperty(key)) {
+        result[key] = 1;
+      }
+    }
+
+    return result;
+  }
+});
