aa07390a716ea7e5ae7a98f9dca2f1fec7f24997
anvaka <anvaka@gmail.com>
2015-09-06 22:47:08 -0700

    Updated dump.js to use stream forEachPackage

--- dump.js
+++ dump.js
@@ -6,21 +6,22 @@ var outFileName = 'online/npmrank.json';
 
 console.log('Collecting tags...');
 var collectTags = require('./lib/collectTags.js');
-var result = collectTags('./data/byField.in.graph');
-var tags = result.tags;
-console.log('Collected ' + result.totalTags + '; Unique tags: ' + result.uniqueTags);
+collectTags('./data/byField.in.graph', function(result) {
+  var tags = result.tags;
+  console.log('Collected ' + result.totalTags + '; Unique tags: ' + result.uniqueTags);
 
-console.log('Computing page rank...');
-var computePageRank = require('./lib/computePageRank.js');
-var rank = computePageRank('./data/dependenciesGraph.out.graph');
-var keys = Object.keys(rank);
-for (var i = 0; i < keys.length; ++i) {
-  rank[keys[i]] = (rank[keys[i]] * 100).toFixed(7);
-}
-console.log('Done');
-console.log('Saving pagerank and tags to ' + outFileName);
+  console.log('Computing page rank...');
+  var computePageRank = require('./lib/computePageRank.js');
+  var rank = computePageRank('./data/dependenciesGraph.out.graph');
+  var keys = Object.keys(rank);
+  for (var i = 0; i < keys.length; ++i) {
+    rank[keys[i]] = (rank[keys[i]] * 100).toFixed(7);
+  }
+  console.log('Done');
+  console.log('Saving pagerank and tags to ' + outFileName);
 
-fs.writeFileSync(outFileName, JSON.stringify({
-  tags: tags,
-  rank: rank
-}));
+  fs.writeFileSync(outFileName, JSON.stringify({
+    tags: tags,
+    rank: rank
+  }));
+});
