598ee14cbc60afd82779a28e7f93ba481a159779
anvaka <anvaka@gmail.com>
2015-09-25 21:22:34 -0700

    Use correct cwd

--- make_snapshot.sh
+++ make_snapshot.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+cd "$(dirname "$0")"
 curl https://skimdb.npmjs.com/registry/_design/scratch/_view/byField > ./data/byField.in.graph
 node saveStats.js ./data/dependenciesGraph.out.graph 1000
 cd ./export/8e8fa57c7ee1350e3491
