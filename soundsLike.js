6f355c9dea26a2b898cb275ea5749636d90ae052
anvaka <anvaka@gmail.com>
2016-03-27 13:07:28 -0700

    maxDistance has inclusive match
    This is more intuitive way to represent edit distance between two
    strings

--- soundsLike.js
+++ soundsLike.js
@@ -1,9 +1,9 @@
 /**
  * Prints all packages that has short Levenshtein distance with a given name
  * E.g.
- *   node soundsLike.js ngraph 2
+ *   node soundsLike.js ngraph 1
  *
- * This will print all package names with distance of 2 edits from ngraph:
+ * This will print all package names with distance of 1 edits from ngraph:
  *
  * dgraph egraph graph igraph jgraph ograph vgraph wgraph
  *
@@ -18,31 +18,36 @@ if (!packageName) {
 }
 
 var distance = require('fast-levenshtein');
+
 var inputFileName = './data/byField.in.graph';
 
-var maxDistance = getMaxDistance();
+// maxDistance is integer value that represents a threshold for Levenshtein's
+// distance: Any package name that has Levenshtein distance between `packageName`
+// larger than `maxDistance` is ignored.
+var maxDistance = parseMaxDistance();
 
 var forEachPackage = require('./lib/forEachPackage.js');
 console.log('Reading packages from ' + inputFileName + '...');
-forEachPackage(inputFileName, rememberPackage, printResults)
+forEachPackage(inputFileName, visitPackage);
 
-function rememberPackage(pkg) {
+function visitPackage(pkg) {
+  // ignore if this our own package
   if (pkg.id === packageName) return;
 
+  // Otherwise get edit distance between our package and current package
   var diff = distance.get(pkg.id, packageName);
-  if (diff < maxDistance) {
+
+  if (diff <= maxDistance) {
+    // found a candidate!
     console.log(pkg.id);
   }
-}
-
-function printResults() {
 }
 
 function printUsage() {
   console.log('soundsLike.js - prints packages that sounds like a given one');
   console.log('');
   console.log('Usage:');
-  console.log('  node soundsLike.js packageName [maxDistance = 3]');
+  console.log('  node soundsLike.js packageName [maxDistance = 2]');
   console.log('');
   console.log('Example:');
   console.log('  node soundsLike.js ngraph 2');
@@ -55,13 +60,13 @@ function printUsage() {
   console.log(' See https://en.wikipedia.org/wiki/Levenshtein_distance for more info');
 }
 
-function getMaxDistance() {
+function parseMaxDistance() {
   var maxDistance = process.argv[3];
 
   if (maxDistance !== undefined) {
     maxDistance = parseInt(maxDistance, 10)
   } else {
-    maxDistance = 3
+    maxDistance = 2
   }
 
   if (isNaN(maxDistance)) {
@@ -69,6 +74,13 @@ function getMaxDistance() {
     console.log('');
     printUsage();
     process.exit(102);
+  }
+
+  if (maxDistance < 1) {
+    console.log('ERROR: maxDistance value is supposed to be greater than zero')
+    console.log('');
+    printUsage();
+    process.exit(103);
   }
 
   return maxDistance;
