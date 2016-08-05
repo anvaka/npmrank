899720115da598ee10913dde6a76694d0a6a613e
anvaka <anvaka@gmail.com>
2016-08-04 23:22:59 -0700

    Added retries
    Hopefully this should reduce a chance for https://github.com/anvaka/npmrank/issues/14

--- make_snapshot.sh
+++ make_snapshot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 # This package generates data for https://gist.github.com/anvaka/8e8fa57c7ee1350e3491
 cd "$(dirname "$0")"
-curl https://skimdb.npmjs.com/registry/_design/scratch/_view/byField > ./data/byField.in.graph && \
+curl --retry 10 https://skimdb.npmjs.com/registry/_design/scratch/_view/byField > ./data/byField.in.graph && \
 /usr/local/bin/node ./convertToGraph.js && \
 /usr/local/bin/node ./saveStats.js ./data/dependenciesGraph.out.graph 1000 && \
 cd ./export/8e8fa57c7ee1350e3491 && \
