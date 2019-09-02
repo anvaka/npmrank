bc2847b6fb8bd92eb9fa7e13ae822041f10c119d
Andrei Kashcha <anvaka@gmail.com>
2019-09-02 06:33:55 -0700

    updated recommendation code

--- recommend.js
+++ recommend.js
@@ -2,35 +2,38 @@ var prompt = require('prompt');
 
 console.log('Collecting tags...');
 var collectTags = require('./lib/collectTags.js');
-var result = collectTags('./data/byField.in.graph');
-var tags = result.tags;
-console.log('Collected ' + result.totalTags + '; Unique tags: ' + result.uniqueTags);
+collectTags('./data/graph.in', process);
+
+function process(result) {
+  var tags = result.tags;
+  console.log('Collected ' + result.totalTags + '; Unique tags: ' + result.uniqueTags);
 
 
-console.log('Computing page rank...');
-var computePageRank = require('./lib/computePageRank.js');
-var rank = computePageRank('./data/dependenciesGraph.out.graph');
-console.log('Done');
+  console.log('Computing page rank...');
+  var computePageRank = require('./lib/computePageRank.js');
+  var rank = computePageRank('./data/dependenciesGraph.out.graph');
+  console.log('Done');
 
-prompt.start();
-getNextInput();
+  prompt.start();
+  getNextInput();
 
-function getNextInput() {
-  prompt.get([{
-    description: 'Enter package keyword',
-    name: 'keyword'
-  }], function (err, result) {
-    var matches = (tags[result.keyword] || []).sort(byPagerank);
-    console.log('----------------------------------------------------------------------');
-    console.log(matches.slice(0, Math.min(20, matches.length)).map(toDisplay).join('\n'));
-    getNextInput();
-  });
-}
+  function getNextInput() {
+    prompt.get([{
+      description: 'Enter package keyword',
+      name: 'keyword'
+    }], function (err, result) {
+      var matches = (tags[result.keyword] || []).sort(byPagerank);
+      console.log('----------------------------------------------------------------------');
+      console.log(matches.slice(0, Math.min(20, matches.length)).map(toDisplay).join('\n'));
+      getNextInput();
+    });
+  }
 
-function toDisplay(x) {
-  return 'https://www.npmjs.com/package/' + x + ' - ' + rank[x];
-}
+  function toDisplay(x) {
+    return 'https://www.npmjs.com/package/' + x + ' - ' + rank[x];
+  }
 
-function byPagerank(x, y) {
-  return rank[y] - rank[x];
+  function byPagerank(x, y) {
+    return rank[y] - rank[x];
+  }
 }
