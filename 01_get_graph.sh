e05a154ca69d14a9756ba8b6aca525d723e75fca
Andriy Kashcha <anvaka@gmail.com>
2020-10-31 11:53:13 -0700

    added error message

--- 01_get_graph.sh
+++ 01_get_graph.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-curl https://skimdb.npmjs.com/registry/_design/scratch/_view/byField > ./data/byField.in.graph
+echo "This will not work :(. Need to find a better way of downloading registry"
+# curl https://skimdb.npmjs.com/registry/_design/scratch/_view/byField > ./data/byField.in.graph
